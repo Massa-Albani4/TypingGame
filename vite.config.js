2cb647569cb3f585df13fc1cd90694c869b35fa7
Massa-Albani4 <massa.albani0213@gmail.com>
2024-09-18 18:35:41 +0300

    deploy app

--- vite.config.js
+++ vite.config.js
@@ -3,9 +3,6 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  base: '/TypingGame/', // Replace 'your-repository-name' with your actual repository name
-  build: {
-    outDir: 'docs', // GitHub Pages uses 'docs' folder for hosting
-  },
+  base: "/TypingGame/",
+  plugins: [react()], // Replace 'your-repository-name' with your actual repository name
 });
